d98b1bfef763084b66ea99d410ba5446b96277d5
E. Choroba <choroba@matfyz.cz>
2022-12-06 00:01:35 +0100

    Solve day 5 in Python
    Interestingly, the two tasks of day 5 in Perl differ only in one
    word ("reverse" is missing in 05b). In Python, the difference is one
    character ("0" versus "i")!

---
+++ 05b.py
@@ -0,0 +1,24 @@
+#! /usr/bin/python3
+import sys
+import re
+
+stacks = []
+with open(sys.argv[1], 'r') as fh:
+    for line in fh:
+        if re.search(r'\d', line):
+            break
+        i = 0
+        for c in line[1::4]:
+            if i >= len(stacks):
+                stacks.append([])
+            if c != ' ':
+                stacks[i].append(c)
+            i += 1
+
+    next(fh)
+    for line in fh:
+        quantity, source, target = map(int, re.findall(r'\d+', line))
+        for i in range(0, quantity):
+            stacks[target - 1].insert(i, stacks[source - 1].pop(0))
+
+print("".join(map(lambda s:s[0], stacks)))
