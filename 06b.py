98b4be1eb36e45026b08a767b44a81c212a7771a
E. Choroba <choroba@matfyz.cz>
2022-12-06 15:42:07 +0100

    Solve 06 in Python

---
+++ 06b.py
@@ -0,0 +1,17 @@
+#! /usr/bin/python3
+import sys
+import warnings
+
+LENGTH = 14
+
+with open(sys.argv[1], 'r') as fh:
+    for line in fh:
+        pos = LENGTH
+        while pos < len(line):
+            s = set(line[pos - LENGTH:pos])
+            if len(s) == LENGTH:
+                print(pos)
+                break
+            pos += 1
+        else:
+            warnings.warn('NOT FOUND')
