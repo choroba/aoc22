9f02fd94a7efc763f6cb9ef6ff959b4f93acfdaa
E. Choroba <choroba@matfyz.cz>
2022-12-20 17:53:37 +0100

    Solve day 20: Grove Positioning System

---
+++ 20a.pl
@@ -0,0 +1,76 @@
+#! /usr/bin/perl
+use warnings;
+use strict;
+use feature qw{ say };
+
+use ARGV::OrDATA;
+
+use enum qw( VALUE PREV NEXT );
+
+my @numbers = (my $first = []);
+my $zero;
+my $next = $first;
+
+sub show {
+    my $start = shift;
+    my $node = $start;
+    do {
+        print $node->[VALUE], ' ';
+        $node = $node->[NEXT];
+    } until $node == $start;
+    print "\n";
+}
+
+while (my $number = <>) {
+    chomp $number;
+    $zero = $next if 0 == $number;
+    $next->[VALUE] = $number;
+    $next->[NEXT] = [undef, $next, []];
+    $next = $next->[NEXT];
+    push @numbers, $next;
+}
+
+pop @numbers;
+$first->[PREV] = $next->[PREV];
+$next->[PREV][NEXT] = $first;
+undef $next;
+
+for my $n (@numbers) {
+    my $v = $n->[VALUE];
+
+    my $shift = abs($v) % (@numbers - 1) or next;
+
+    --$shift if $v < 0;
+
+    my $direction = $v > 0 ? NEXT : PREV;
+    my $target = $n->[$direction];
+
+    $n->[PREV][NEXT] = $n->[NEXT];
+    $n->[NEXT][PREV] = $n->[PREV];
+
+    $target = $target->[$direction] for 1 .. $shift;
+
+    $n->[NEXT] = $target;
+    $n->[PREV] = $target->[PREV];
+    $target->[PREV][NEXT] = $n;
+    $target->[PREV] = $n;
+}
+
+my $n = $zero;
+my $step = 1000 % @numbers;
+my $sum = 0;
+for (1 .. 3) {
+    $n = $n->[NEXT] for 1 .. $step;
+    say $n->[VALUE];
+    $sum += $n->[VALUE];
+}
+say $sum;
+
+__DATA__
+1
+2
+-3
+3
+-2
+0
+4
